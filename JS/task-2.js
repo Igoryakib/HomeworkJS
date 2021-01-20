9b0b6557cdc15c1bc1cd065b042c9e5d3fd4a32d
Igoryakib <Igoryakib@gmail.com>
2021-01-20 20:50:35 +0200

    added

---
+++ JS/task-2.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let age = 5;
+
+console.log(age)
+
+let yourName = 'Igor';
+
+console.log(yourName)
+
+let iscar = false;
+
+console.log(iscar)
+
+let laptopName = 'Lenovo';
+
+console.log(laptopName)
+
+let isflat = true;
+
+console.log(isflat)
+
+
+
+const number = 10;
+
+console.log(number)
+
+const txt = 'hi'
+
+console.log(txt)
+
+const iswindow = true;
+
+console.log(iswindow)
+
+const num = 19;
+
+console.log(num)
+
+const str = '55';
