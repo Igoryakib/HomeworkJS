cbc280bc6b1339df1d36fc68d7742f74397b3741
Igoryakib <Igoryakib@gmail.com>
2021-01-22 15:30:15 +0200

    added

--- Module1/JS/task-1.js
+++ Module1/JS/task-1.js
@@ -1,4 +1,20 @@
-'use strict';
+"use strict";
+
+const str = "true";
+
+console.log(typeof str);
+
+const num = 123;
+
+console.log(typeof num);
+
+const isflag = true;
+
+console.log(typeof isflag);
+
+const txt = "Привіт";
+
+console.log(typeof txt);
 
 let age = 5;
 
