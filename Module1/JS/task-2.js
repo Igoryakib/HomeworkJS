c0922811dcd0d23954faf9aa0a9eb250259804e5
Igoryakib <Igoryakib@gmail.com>
2021-01-24 22:03:50 +0200

    added

--- Module1/JS/task-2.js
+++ Module1/JS/task-2.js
@@ -1,94 +1,94 @@
-"use strict";
-// 1
-const userName = prompt("Your name ?");
+// "use strict";
+// // 1
+// const userName = prompt("Your name ?");
 
-console.log(userName);
+// console.log(userName);
 
-// 2
-const myAge = 14;
+// // 2
+// const myAge = 14;
 
-alert(myAge);
+// alert(myAge);
 
-const friendAge = 16;
+// const friendAge = 16;
 
-alert(friendAge);
+// alert(friendAge);
 
-// 3
-const a = prompt('Number1 -');
+// // 3
+// const a = prompt('Number1 -');
 
-const b = prompt('Number2 -');
+// const b = prompt('Number2 -');
 
-const x = Number(a);
+// const x = Number(a);
 
-const y = Number(b);
+// const y = Number(b);
 
-console.log(x + y);
+// console.log(x + y);
 
-// 4
-const a1 = 5 + 3;
+// // 4
+// const a1 = 5 + 3;
 
-console.log(a1);
+// console.log(a1);
 
-const a2 = 5 - 3;
+// const a2 = 5 - 3;
 
-console.log(a2);
+// console.log(a2);
 
-const a3 = 5 * 3;
+// const a3 = 5 * 3;
 
-console.log(a3);
+// console.log(a3);
 
-const a4 = 5 / 3;
+// const a4 = 5 / 3;
 
-console.log(a4)
+// console.log(a4)
 
-const a5 = 5 % 3;
+// const a5 = 5 % 3;
 
-console.log(a5);
+// console.log(a5);
 
-// 5
-const a6 = 5 % 3;
+// // 5
+// const a6 = 5 % 3;
 
-alert(a6);
+// alert(a6);
 
-const a7 = 3 % 5;
+// const a7 = 3 % 5;
 
-alert(a7);
+// alert(a7);
 
-const a8 = 5 + '3';
+// const a8 = 5 + '3';
 
-alert(a8);
+// alert(a8);
 
-const a9 = '5' - 3;
+// const a9 = '5' - 3;
 
-alert(a9);
+// alert(a9);
 
-const a10 = 75 + 'кг';
+// const a10 = 75 + 'кг';
 
-alert(a10);
+// alert(a10);
 
-// 6
-const height = 23;
+// // 6
+// const height = 23;
 
-const width = 10;
+// const width = 10;
 
-const s = height * width;
+// const s = height * width;
 
-console.log(s);
+// console.log(s);
 
-// 7
-const heightC = 10;
+// // 7
+// const heightC = 10;
 
-const dC = 4;
+// const dC = 4;
 
-const v = heightC * dC * (3.14 * 100) / 100;
+// const v = heightC * dC * (3.14 * 100) / 100;
 
-console.log(v);
+// console.log(v);
 
-// 8
-const n = 3;
+// // 8
+// const n = 3;
 
-const m = 4;
+// const m = 4;
 
-const k = (Math.pow(3, 2) + Math.pow(4, 2)) / 5;
+// const k = (Math.pow(3, 2) + Math.pow(4, 2)) / 5;
 
-console.log(k);
+// console.log(k);
