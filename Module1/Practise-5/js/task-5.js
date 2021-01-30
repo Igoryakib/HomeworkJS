450ac834d8dcc080afc056fc260aaeac00a02f5b
Igoryakib <Igoryakib@gmail.com>
2021-01-30 15:48:49 +0200

    fix

--- Module1/Practise-5/js/task-5.js
+++ Module1/Practise-5/js/task-5.js
@@ -30,7 +30,7 @@ let heroName = '';
 
 do {
     heroName = prompt('Ваш улюблений герой менше 6 букв ');
-    console.log('Ваш герой ', heroName);11
+    console.log('Ваш герой ', heroName);
     if (heroName.length > 6) {
         console.log('Задовге імя');
         break
