93051a7c89707d55ce572d17ae9e270d112b5fdd
Igoryakib <Igoryakib@gmail.com>
2021-02-07 15:00:57 +0200

    fix

--- Module2/Practise-work-1/js/task-1.js
+++ Module2/Practise-work-1/js/task-1.js
@@ -4,6 +4,6 @@ const Arr1 = [1, 5, "4", "hello"];
 
 const Arr2 = [true, 2, {}, ["a"], 222];
 
-console.log('Arr1', Arr1[2]);
-console.log('Arr2', Arr2[2]);
-console.log('Sum', Arr1[2] + Arr2[2]);
+console.log('Arr1', Arr1[1]);
+console.log('Arr2', Arr2[1]);
+console.log('Sum', Arr1[1] + Arr2[1]);
