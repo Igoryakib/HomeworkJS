344b2abd724efad9cb7f7a13d80e81d0a6642146
Igoryakib <Igoryakib@gmail.com>
2021-02-25 17:25:43 +0200

    fix

--- Module3/Practise-work1/js/task-3.js
+++ Module3/Practise-work1/js/task-3.js
@@ -1,3 +1,5 @@
+
+// 1 спосіб
 // const calculator = {
 //     a: prompt('Введіть 1 число'),
 //     b: prompt('Введіть 2 число'),
@@ -14,28 +16,51 @@
 //     },
 // };
 
-const calculator = {
-    read1(a) {
-        a = prompt('Введіть 1 число');
-        return Number(a);
-    },
+// 2 спосіб
 
-    read2(b) {
-        b = prompt('Введіть 2 число');
-        return Number(b);
-    },
+// const calculator = {
+//     read1(a) {
+//         a = prompt('Введіть 1 число');
+//         return Number(a);
+//     },
+
+//     read2(b) {
+//         b = prompt('Введіть 2 число');
+//         return Number(b);
+//     },
+
+//     sum() {
+        
+//         const sum = this.read1() + this.read2();
+//         return sum;
+//     },
+
+//     mult() {
+//         const total = this.read1() * this.read2();
+//         return total;
+//     },
+// };
+
+// 3 спосіб 
+
+const a = prompt('Вкажіть 1 число -');
+const b = prompt('Вкажіть 2 число -');
+const calculator = {
+    a,
+    b,
 
     sum() {
         
-        const sum = this.read1() + this.read2();
+        const sum = Number(this.a) + Number(this.b);
         return sum;
     },
 
     mult() {
-        const total = this.read1() * this.read2();
+        const total = Number(this.a) * Number(this.b);
         return total;
     },
 };
+
 
 console.log('Сума -', calculator.sum());
 
