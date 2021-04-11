27e36f9f0b317ca45d56cd350383bca4e0bee998
Igoryakib <Igoryakib@gmail.com>
2021-04-11 21:44:48 +0300

    fix

--- Module8/js/task.js
+++ Module8/js/task.js
@@ -23,8 +23,7 @@ const createElementsFn = (item) => {
     imgRef.setAttribute("src", item.preview);
     imgRef.setAttribute("data-source", item.original);
     imgRef.setAttribute("alt", item.description);
-    aRef.setAttribute('data-index', idx);
-    imgRef.setAttribute('data-index', idx);
+    imgRef.setAttribute("data-index", idx);
     aRef.appendChild(imgRef);
     liRef.append(aRef);
     return ulRef.appendChild(liRef);
@@ -34,39 +33,67 @@ const createElementsFn = (item) => {
 
 console.log(createElementsFn(gallery));
 
+let nextPicture = +modalImgRef.getAttribute('data-index');
+let previousPicture = gallery.length;
 
 const keyNextPictureFn = (event) => {
-    if (event.code === 'ArrowRight') {
-       const nextImg = document.querySelector(`[data-index = '${+event.target.dataset.index + 1}']`);
-      return  modalImgRef.setAttribute('src', nextImg.getAttribute('data-source'));
+  nextPicture += 1;
+  if (event.code === "ArrowRight") {
+    if (nextPicture === 9) {
+      nextPicture = 0;
+      const firstImgRef = document.querySelector(`img[data-index = '0']`);
+      return modalImgRef.setAttribute(
+        "src",
+        firstImgRef.getAttribute("data-source")
+      );
     }
-
-    if (event.code === 'ArrowLeft') {
-      const nextPicture = +event.target.dataset.index - 1;
-        if (nextPicture === -1) {
-          const lastImg = document.querySelector(`img[data-index = '8']`);
-          return modalImgRef.setAttribute('src', lastImg.getAttribute('data-source'));
-        }
-        const previousImg = document.querySelector(`img[data-index = '${+event.target.dataset.index - 1}']`);
-       modalImgRef.setAttribute('src', previousImg.getAttribute('data-source'));
-    }
+    const nextImg = document.querySelector(
+      `img[data-index = '${nextPicture}']`
+    );
+    return modalImgRef.setAttribute("src", nextImg.getAttribute("data-source"));
+  }
 };
 
+const keyPreviousPicture = (event) => {
+  if (event.code === "ArrowLeft") {
+    previousPicture -= 1;
+    if (previousPicture === -1) {
+      previousPicture = gallery.length;
+      const lastImgRef = document.querySelector(`img[data-index = '8']`);
+      return modalImgRef.setAttribute(
+        "src",
+        lastImgRef.getAttribute("data-source")
+      );
+    }
+    const previousImg = document.querySelector(
+      `img[data-index = '${previousPicture}']`
+    );
+    return modalImgRef.setAttribute(
+      "src",
+      previousImg.getAttribute("data-source")
+    );
+  }
+};
 
 const openModal = (event) => {
   event.preventDefault();
   modalWindow.classList.add("is-open");
-  modalImgRef.setAttribute('src', event.target.getAttribute('data-source'));
-  modalImgRef.setAttribute('alt', event.target.getAttribute('alt'));
+  modalImgRef.setAttribute("src", event.target.getAttribute("data-source"));
+  modalImgRef.setAttribute("alt", event.target.getAttribute("alt"));
+  modalImgRef.setAttribute("data-index", event.target.getAttribute("data-index"));
   window.addEventListener("keydown", keyCloseModal);
-  window.addEventListener('keydown', keyNextPictureFn);
+  window.addEventListener("keydown", keyNextPictureFn);
+  window.addEventListener("keydown", keyPreviousPicture);
 };
 
 const closeModal = () => {
+  nextPicture = +modalImgRef.getAttribute('data-index');
+  previousPicture = gallery.length;
   modalWindow.classList.remove("is-open");
   modalImgRef.removeAttribute("src");
   window.removeEventListener("keydown", keyCloseModal);
-  window.removeEventListener('keydown', keyNextPictureFn)
+  window.removeEventListener("keydown", keyNextPictureFn);
+  window.removeEventListener("keydown", keyPreviousPicture);
 };
 
 const backdropCloseModal = (event) => {
